c83c56739e88c53f7a087da5b9c2fb2169522ecf
Homma <ab13891012.gmail.com>
2016-08-11 08:48:58 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DomoApp::Application.config.secret_key_base = '1640667d6eb24360a004622092618d586e803f7410412d97a62a4d9a0e9bd99d3c0537a4b2dd07571a617e580ba23a4d1fd385e1f2e170c586912cfc68339105'
